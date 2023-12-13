845a0b60095e9f9494c1ff04d60b0aeb0a837738
Coen Warmer <coen.warmer@gmail.com>
2023-12-13 15:37:35 +0100

    Test

--- x-pack/plugins/observability_ai_assistant/public/plugin.tsx
+++ x-pack/plugins/observability_ai_assistant/public/plugin.tsx
@@ -38,6 +38,7 @@ export class ObservabilityAIAssistantPlugin
       ObservabilityAIAssistantPluginStartDependencies
     >
 {
+  // poop
   logger: Logger;
   service?: ObservabilityAIAssistantService;
 
